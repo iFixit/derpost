7b1a8c8825a7c62df9db3ba683dde9ff9d0a90e0
James Pearson <james@ifixit.com>
2012-05-08 21:46:20 -0700

    initial hacking out of stuff onto screen

---
+++ derpost.rb
@@ -0,0 +1,23 @@
+#!/usr/bin/env ruby
+
+messages = `postqueue -p | sed 1d | ../lineify/lineify.rb`.split "\n"
+pageStart = 0
+
+while true
+	if pageStart < messages.size
+		messages[pageStart, 10].each_with_index do |message, index|
+			puts "#{index+1}: #{message}"
+		end
+	else
+		puts 'End of messages.'
+	end
+	
+	print '[$]> '
+	case gets.strip
+	when 'n'
+		pageStart += 10
+	when 'q'
+		break
+	end
+end
+
