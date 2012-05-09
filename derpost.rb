978ef6a7e018c43b55809a389bd58d054f696cff
James Pearson <james@ifixit.com>
2012-05-08 23:06:57 -0700

    extract out index printing

--- derpost.rb
+++ derpost.rb
@@ -1,9 +1,6 @@
 #!/usr/bin/env ruby
 
-messages = `postqueue -p | sed 1d | ../lineify/lineify.rb`.split "\n"
-pageStart = 0
-
-while true
+def print_index(messages, pageStart)
 	if pageStart < messages.size
 		messages[pageStart, 10].each_with_index do |message, index|
 			puts "#{index+1}: #{message}"
@@ -11,11 +8,19 @@ while true
 	else
 		puts 'End of messages.'
 	end
-	
+end
+
+messages = `postqueue -p | sed 1d | ../lineify/lineify.rb`.split "\n"
+pageStart = 0
+
+print_index messages, pageStart
+while true
 	print '[$]> '
-	case gets[0]
+	command = gets
+	case command[0]
 	when 'n'
 		pageStart += 10
+		print_index messages, pageStart
 	when 'q'
 		break
 	end
